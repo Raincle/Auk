cf4127420898fdb32d52e28fd4daad4e23d26965
Hong <hongyawei@yit.com>
2017-11-23 10:28:39 +0800

    TransferredFromRobot

---
+++ yit_service_transferred_from_robot.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Yit Mark TransferredFromRobot
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Hong
+// @match        https://kefu.easemob.com/mo/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
