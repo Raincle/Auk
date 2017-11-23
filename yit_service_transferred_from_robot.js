dee0b26fe820f721605610c9454b9ac9ced7d366
Hong <hongyawei@yit.com>
2017-11-23 10:33:02 +0800

    init

--- yit_service_transferred_from_robot.js
+++ yit_service_transferred_from_robot.js
@@ -11,5 +11,94 @@
 (function() {
     'use strict';
 
-    // Your code here...
+    !function (ob) {
+        ob.hookAjax = function (funs) {
+            window._ahrealxhr = window._ahrealxhr || XMLHttpRequest;
+            XMLHttpRequest = function () {
+                this.xhr = new window._ahrealxhr;
+                for (var attr in this.xhr) {
+                    var type = "";
+                    try {
+                        type = typeof this.xhr[attr];
+                    } catch (e) {}
+                    if (type === "function") {
+                        this[attr] = hookfun(attr);
+                    } else {
+                        Object.defineProperty(this, attr, {
+                            get: getFactory(attr),
+                            set: setFactory(attr)
+                        });
+                    }
+                }
+            };
+
+            function getFactory(attr) {
+                return function () {
+                    return this.hasOwnProperty(attr + "_")?this[attr + "_"]:this.xhr[attr];
+                };
+            }
+
+            function setFactory(attr) {
+                return function (f) {
+                    var xhr = this.xhr;
+                    var that = this;
+                    if (attr.indexOf("on") !== 0) {
+                        this[attr + "_"] = f;
+                        return;
+                    }
+                    if (funs[attr]) {
+                        xhr[attr] = function () {
+                            funs[attr](that) || f.apply(xhr, arguments);
+                        };
+                    } else {
+                        xhr[attr] = f;
+                    }
+                };
+            }
+
+            function hookfun(fun) {
+                return function () {
+                    var args = [].slice.call(arguments);
+                    if (funs[fun] && funs[fun].call(this, args, this.xhr)) {
+                        return;
+                    }
+                    return this.xhr[fun].apply(this.xhr, args);
+                };
+            }
+            return window._ahrealxhr;
+        };
+        ob.unHookAjax = function () {
+            if (window._ahrealxhr)  XMLHttpRequest = window._ahrealxhr;
+            window._ahrealxhr = undefined;
+        };
+    }(window);
+    
+    
+    hookAjax({
+        //hook callbacks
+        onreadystatechange:function(xhr){
+            var url = xhr.responseURL;
+            switch(xhr.readyState){
+                case 1://OPENED
+                    //do something
+                    break;
+                case 2://HEADERS_RECEIVED
+                    //do something
+                    break;
+                case 3://LOADING
+                    //do something
+                    break;
+                case 4://DONE
+                    //do something
+                    if (url.indexOf("Visitors?") > -1) {
+                        var visitors = JSON.parse(xhr.response);
+                        console.log(visitors);
+                    }
+                    break;
+            }
+        }
+    });
+    
+    
+    
 })();
